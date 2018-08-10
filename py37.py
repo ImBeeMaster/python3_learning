f43b0f7ba2139acb878ddf32fea77bd3838b569a
Oleksii Pasichnyk <Oleksii_Pasichnyk@epam.com>
2018-08-10 12:03:41 +0300

    Made files from py36 to py39

---
+++ py37.py
@@ -0,0 +1,19 @@
+# print("How old are you", end=' ')
+# age = input()
+# print("How tall are you", end=' ')
+# height = input()
+# print("How much do tou weight?", end=' ')
+# weight = input()
+# #lets define x
+# print("Enter an integer value for x, please", end=' ')
+# x = int(input())
+# print("Enter an integer value for y, please", end=' ')
+# y = int(input())
+# x = x + y
+# print(f"X now equal to {x}")
+
+# print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+x = int(input("Enter X "))
+y = int(input("Enter Y "))
+print("The sum of", x, "and", y, "is equal to", x+y,'.', sep=' ')
---
+++ py38.py
@@ -0,0 +1,10 @@
+from sys import argv
+script, first, second, third = argv
+# argv[2] = input("Enter a value ")
+# print("We have", argv[2])
+second = int(second)
+third = int(third)
+print("ther script is called", script)
+print("Your first variable is: ", first)
+print("Your secondd variable is:", second+third)
+print("Your third variable is: ", third)
