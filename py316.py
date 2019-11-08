53e0803f08f91ea320ebe6bc6f4357d1a7a2dc8f
ImBeeMaster <36162195+ImBeeMaster@users.noreply.github.com>
2019-11-08 23:37:17 +0200

    .

---
+++ py316.py
@@ -0,0 +1,29 @@
+def add(a, b):
+    print(f"ADDING {a} + {b}")
+    return a + b
+
+def substract(a, b):
+    print(f"SUBTRACTING {a} - {b}")
+    return a - b
+
+def multiply(a, b):
+    print(f"MULTIPLYNG {a} * {b}")
+    return a * b
+
+def divide(a, b):
+    print(f"DIVIDING {a} / {b}")
+    return a / b
+
+print("Let's do some math with fuctions!")
+
+age = add(30, 5)
+height = substract(78, 4)
+weight = multiply(90, 2)
+iq = divide(100, 2)
+
+print(f"Age: {age}, Height: {height}, Weight: {weight}, IQ: {iq}")
+
+print("Here is a puzzle")
+what = add(age, substract(height, multiply(weight, divide(iq, 2))))
+
+print("That becomes: ", what, "Can you do it by hand?")
